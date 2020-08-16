338047e7bd87b98ede03b2630cb9ccdca3c2b21a
Wuvist <wuvist@gmail.com>
2020-08-16 23:47:14 +0800

    Add name card data

---
+++ NameCardData.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class NameCardData {
+  final String name;
+  final String title;
+  final String address;
+  final String tel;
+
+  NameCardData({this.name, this.title, this.address, this.tel});
+
+  factory NameCardData.fromJson(Map<String, dynamic> json) {
+    return NameCardData(
+      name: json['name'],
+      title: json['title'],
+      address: json['address'],
+      tel: json['tel'],
+    );
+  }
+}
+
+Future<NameCardData> fetchNameCard(int id) async {
+  final response =
+      await http.get('https://jsonplaceholder.typicode.com/namecard/$id');
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return NameCardData.fromJson(json.decode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
