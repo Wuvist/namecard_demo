52b587af6044ee243b6687016397c379e66d3547
ZhouCihang <matthewzhou1128@gmail.com>
2020-08-16 14:54:38 +0800

    add row

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,68 @@ class NameCardBasic extends StatelessWidget{
           width: 370,
           height: 250,
           color: Colors.blue,
-          
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Icon(Icons.child_friendly, size: 65),
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Flutter McFlutter',
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),),
+                      //Text('Flutter McFlutter', style: Theme.of(context).textTheme.headline,),
+                      //Text('Experienced App Developer'),
+                      Text('Experienced App Developer',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text('123 Main Street',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),),
+                  Text('(415) 555-0198',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Icon(Icons.accessibility, size: 40,),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Icon(Icons.timer, size: 40,),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Icon(Icons.phone_android, size: 40,),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Icon(Icons.phone_iphone, size: 40,),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
