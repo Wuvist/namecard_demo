447c90d0913cb81401f20bb4fd0cae7a29274131
Wuvist <wuvist@gmail.com>
2020-08-29 16:55:53 +0800

    Refactor widget into NameCardDetail

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'NameCardData.dart';
+import 'NameCardDetail.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,125 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: NameCardBasic(),
-    );
-  }
-}
-
-class NameCardBasic extends StatefulWidget {
-  @override
-  _NameCardBasicState createState() => new _NameCardBasicState();
-}
-
-class _NameCardBasicState extends State<NameCardBasic> {
-  Future<NameCardData> futureNameCardData;
-
-  @override
-  void initState() {
-    super.initState();
-    futureNameCardData = fetchNameCard(1);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Align(
-        child: Container(
-          width: 370,
-          height: 250,
-          color: Colors.blue,
-          child: FutureBuilder<NameCardData>(
-              future: futureNameCardData,
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Text("Loading...");
-                }
-
-                return Column(
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Icon(Icons.child_friendly, size: 65),
-                        ),
-                        Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              snapshot.data.name,
-                              style: TextStyle(
-                                fontSize: 30,
-                              ),
-                            ),
-                            Text(
-                              snapshot.data.title,
-                              style: TextStyle(
-                                fontSize: 20,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 12),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(
-                          snapshot.data.address,
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          snapshot.data.tel,
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 30),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Icon(
-                            Icons.accessibility,
-                            size: 40,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Icon(
-                            Icons.timer,
-                            size: 40,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Icon(
-                            Icons.phone_android,
-                            size: 40,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Icon(
-                            Icons.phone_iphone,
-                            size: 40,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                );
-              }),
-        ),
-      ),
+      home: NameCardDetail(),
     );
   }
 }
